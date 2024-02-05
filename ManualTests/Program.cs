cb6d78ada754d67c06cdb1eee09e82c1e4925863
ms <geophizz@hotmail.com>
2024-02-04 21:44:40 -0500

    Added code to awaity for a keystroke to quit

--- ManualTests/Program.cs
+++ ManualTests/Program.cs
@@ -6,6 +6,7 @@ try
 {
 	Photo_Tools.SQLite_Handler dbTester = new SQLite_Handler($"D:/Scratch/PhotoTools.db", $"D:/Scratch/CREATE TABLE PhotoList .txt");
 	Console.WriteLine("Connecting with specific location and SQL Code - Works");
+    Console.WriteLine($"Press any key to quit");Console.Read();
 }
 catch (Exception)
 {
