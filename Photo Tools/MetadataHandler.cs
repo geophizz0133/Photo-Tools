35ddd111d9e9b652fc5dc5ca0c5b262f878eea3f
ms <geophizz@hotmail.com>
2024-11-24 08:55:13 -0500

    Added more fields

--- Photo Tools/MetadataHandler.cs
+++ Photo Tools/MetadataHandler.cs
@@ -29,67 +29,71 @@ namespace Photo_Tools
             photoData.Extension = fileInfo.Extension;
             photoData.FileSize = fileInfo.Length.ToString();
 
-            var directories = ImageMetadataReader.ReadMetadata(PhotoFileLocation);
+           
+                var directories = ImageMetadataReader.ReadMetadata(PhotoFileLocation);
 
-            //Cycle thru all directories
-            foreach (var directory in directories)
-            { 
-                foreach (var MetadataTag in directory.Tags)
+                //Cycle thru all directories
+                foreach (var directory in directories)
                 {
-                    switch (MetadataTag.Description) 
+                    foreach (var MetadataTag in directory.Tags)
                     {
-                        case "Reduced-Resolution Image":
-                            photoData.ReducedResolution = true;
-                            break;
-                    }
-                    switch(MetadataTag.Name) 
-                    {
-                        case "Make":
-                            photoData.CameraMake = MetadataTag.Description;
-                            break;
-                        case "Model":
-                            photoData.CameraModel = MetadataTag.Description;
-                            break;
-                        case "F-Number":
-                            photoData.fStop = MetadataTag.Description;
-                            break;
-                        case "Focal Length":
-                            photoData.FocalLength = MetadataTag.Description;
-                            break;
-                        case "Date/Time":
-                        case "Date/Time Digitized":
-                        case "Date/Time Original":  //this needs more handling
-                            photoData.DateCaptured = MetadataTag.Description;
-                            break;
-                        case "Exposure Time":
-                            photoData.ShutterSpeed = MetadataTag.Description;
-                            break;
-                        case "Software":
-                            photoData.Software = MetadataTag.Description;
-                            break;
-                        case "Image Height":
-                        case "Pixel Y Dimension":
-                            photoData.ImageHeight = MetadataTag.Description;
-                            break;
-                        case "Image Width":
-                        case "Pixel X Dimension":
-                            photoData.ImageWidth = MetadataTag.Description;
-                            break;
-                        case "Full Image Size":
-                            photoData.FullImageSize = MetadataTag.Description;
-                            break;
-                        case "File Modified Date":
-                            photoData.DateLastModified = MetadataTag.Description;
-                            break;
-                        default:
-                            break;
-                    
-                    }
+                        switch (MetadataTag.Description)
+                        {
+                            case "Reduced-Resolution Image":
+                                photoData.ReducedResolution = true;
+                                break;
+                        }
+                        switch (MetadataTag.Name)
+                        {
+                            case "Make":
+                                photoData.CameraMake = MetadataTag.Description;
+                                break;
+                            case "Model":
+                                photoData.CameraModel = MetadataTag.Description;
+                                break;
+                            case "F-Number":
+                                photoData.fStop = MetadataTag.Description;
+                                break;
+                            case "Focal Length":
+                                photoData.FocalLength = MetadataTag.Description;
+                                break;
+                            case "Date/Time":
+                            case "Date/Time Digitized":
+                            case "Date/Time Original":  //this needs more handling
+                                photoData.DateCaptured = MetadataTag.Description;
+                                break;
+                            case "Exposure Time":
+                                photoData.ShutterSpeed = MetadataTag.Description;
+                                break;
+                            case "Software":
+                                photoData.Software = MetadataTag.Description;
+                                break;
+                            case "Image Height":
+                            case "Pixel Y Dimension":
+                                photoData.ImageHeight = MetadataTag.Description;
+                                break;
+                            case "Image Width":
+                            case "Pixel X Dimension":
+                                photoData.ImageWidth = MetadataTag.Description;
+                                break;
+                            case "Full Image Size":
+                                photoData.FullImageSize = MetadataTag.Description;
+                                break;
+                            case "File Modified Date":
+                                photoData.DateLastModified = MetadataTag.Description;
+                                break;
+                            default:
+                                if (photoData.Extension.ToLower() == ".png")
+                                {
+                                    Console.WriteLine($"RandoData:{photoData.FilePath} - {MetadataTag.Name}:{MetadataTag.Description}");
+                                }
+                                break;
 
+                        }
+
+                    }
                 }
-            }
-
-            return photoData;
+           return photoData;
         }
     }
 }
