01fe52c07dc88ed01c8c461061ac8274a676e61c
ms <geophizz@hotmail.com>
2024-11-29 11:24:36 -0500

    Added stub class for handling image data (as opposed to metadata)

---
+++ Photo Tools/ImageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo_Tools
+{
+    internal class ImageHandler
+    {
+        public ImageHandler() { }
+        public ImageHandler(string path) { }
+
+        public bool IsMonochrome(string path) { return false; }
+        public decimal GetRGBHash(string path) { return 0; }
+
+        public void SaveImageAs(string path, string newformat) { }
+
+
+    }
+}
